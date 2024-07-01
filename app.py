bcb1c5c56d082e1605180aa808ee570287c4b2c0
Marco Santos <marco.inctva2@gmail.com>
2024-06-30 20:28:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,4 @@ def data():
     return jsonify([])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
